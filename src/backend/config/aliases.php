ae78d986fe970ed9bd33f656f91b6239dc30ddc1
Nolberto Vilchez <jnolbertovm@gmail.com>
2017-12-14 18:40:28 -0500

    remove underscore alias @thirdparty

--- src/backend/config/aliases.php
+++ src/backend/config/aliases.php
@@ -3,6 +3,6 @@
 return [
     '@backend'     => dirname(__DIR__) . '/../src/backend/',
     '@bower'       => '@app/web/static/third_party/',
-    '@third_party' => 'static/third_party/'
+    '@thirdparty' => 'static/third_party/'
 ];
 

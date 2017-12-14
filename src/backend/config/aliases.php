6c8431e9681462a633c10d98bbd45a73d7fc1332
Nolberto Vilchez <jnolbertovm@gmail.com>
2017-12-14 18:37:24 -0500

    SET aliases @bower, @third_party

--- src/backend/config/aliases.php
+++ src/backend/config/aliases.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
-    '@backend' => dirname(__DIR__) . '/../src/backend/',
+    '@backend'     => dirname(__DIR__) . '/../src/backend/',
+    '@bower'       => '@app/web/static/third_party/',
+    '@third_party' => 'static/third_party/'
 ];
 

db7698a09ca99e3cf05fdd9b3917daa5003200f6
Nolberto Vilchez <jnolbertovm@gmail.com>
2017-12-15 08:45:10 -0500

    Remove alias @thirdparty

--- src/backend/config/aliases.php
+++ src/backend/config/aliases.php
@@ -2,7 +2,6 @@
 
 return [
     '@backend'     => dirname(__DIR__) . '/../src/backend/',
-    '@bower'       => '@app/web/static/third_party/',
-    '@thirdparty' => 'static/third_party/'
+    '@bower'       => '@app/web/static/third_party/'
 ];
 
